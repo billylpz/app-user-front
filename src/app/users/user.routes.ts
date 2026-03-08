f1fbdf7a2a3d9f31194b22c1c38c91a0839a8116
billylpz <billyj.96@hotmail.com>
2026-03-08 08:26:20 -0500

    feat: add routes to components

--- src/app/users/user.routes.ts
+++ src/app/users/user.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { UserFrontLayoutComponent } from '../user-front/layouts/user-front-layout/user-front-layout.component';
 import { HomePageComponent } from '../user-front/pages/home-page/home-page.component.component';
+import { UserFormComponent } from '../user-front/pages/user-form/user-form.component';
 
 export   const userRoutes: Routes = [
   {
     path:"", component:UserFrontLayoutComponent,
     children:[
-      {path:"home",component:HomePageComponent},
-      {path:"**",redirectTo:"/home"}
+      {path:"users",component:HomePageComponent},
+      {path:"form",component:UserFormComponent},
+      {path:"form/:id",component:UserFormComponent},
+      {path:"**",redirectTo:"users"}
     ]
     
   }
