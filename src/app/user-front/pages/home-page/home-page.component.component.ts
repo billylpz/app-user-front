b1a25ed196356e7200ccee6bbe567dfe7e5ee010
billylpz <billyj.96@hotmail.com>
2026-03-08 08:24:10 -0500

    feat: add homepage component

--- src/app/user-front/pages/home-page/home-page.component.component.ts
+++ src/app/user-front/pages/home-page/home-page.component.component.ts
@@ -1,18 +1,29 @@
 import { User } from './../../../users/interfaces/user.interface';
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { UserCardComponent } from "../../../users/components/user-card/user-card.component";
+import { rxResource } from '@angular/core/rxjs-interop';
+import { UserService } from '../../../users/services/user.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home-page.component',
   templateUrl: './home-page.component.component.html',
   styleUrls: ['./home-page.component.component.css'],
-  imports: [UserCardComponent]
+  imports: [UserCardComponent, RouterLink]
 })
-export class HomePageComponent  {
-  array=signal( 0);
+export class HomePageComponent {
+  userService = inject(UserService);
 
   constructor() { }
 
- 
+
+  usersResource = rxResource({
+    params: () => ({}),
+    stream: () => {
+      return this.userService.findAll();
+    }
+  });
 
 }
+
+
