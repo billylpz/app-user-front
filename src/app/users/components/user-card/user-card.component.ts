1003a3ef561592709f83181610067d385fd14c49
billylpz <billyj.96@hotmail.com>
2026-03-08 08:25:07 -0500

    feat: add user-card component

--- src/app/users/components/user-card/user-card.component.ts
+++ src/app/users/components/user-card/user-card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, effect, input, OnInit, signal } from '@angular/core';
+import { User } from '../../interfaces/user.interface';
+import { RouterLink } from "@angular/router";
+import { ImageUtils } from '../../../utils/image-utils';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.css']
+  styleUrls: ['./user-card.component.css'],
+  imports: [RouterLink]
 })
-export class UserCardComponent implements OnInit {
+export class UserCardComponent  {
+  user=input<User>();
+  imageUtils=ImageUtils;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+
+   
 
 }
