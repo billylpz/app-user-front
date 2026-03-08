4cae7baa228318f01dc14f007d41768cad048865
billylpz <billyj.96@hotmail.com>
2026-03-08 08:23:04 -0500

    feat: add navbar and footer component

--- src/app/user-front/components/user-front-navbar/user-front-navbar.component.ts
+++ src/app/user-front/components/user-front-navbar/user-front-navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-user-front-navbar',
   templateUrl: './user-front-navbar.component.html',
-  styleUrls: ['./user-front-navbar.component.css']
+  styleUrls: ['./user-front-navbar.component.css'],
+  imports: [RouterLink]
 })
 export class UserFrontNavbarComponent implements OnInit {
 
